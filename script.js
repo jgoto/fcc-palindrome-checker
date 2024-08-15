844c27695d510764ff11c24805c9e26ce8573c7f
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-08-15 09:20:47 -0400

    Added logic to check for palindrome on button click

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
+const result = document.getElementById("resultText");
+const inputArray = [];
+let isPalindrome = false;
 
 /* Ensure user enters a value for the text input 
 when the submit button is clicked */
 checkBtn.addEventListener("click", () => {
     if(textInput.value===""){
         alert("Please fill in a value");
+    }
+    else{
+        const input = textInput.value.replace(/\s/g,"");
+        input.split('').forEach(char => {
+            inputArray.push(char);
+        });
+        evaluatePalindrome(inputArray);
+        result.innerText = inputArray;
     }
 });
 
@@ -17,19 +28,26 @@ checkBtn.addEventListener("click", () => {
     otherwise the input is not a palindrome
 */
 
+
+
+/** strip whitespace and place in array*/
+
+
+
+
 function evaluatePalindrome(array){
+    console.log("checking to see if this is a palindrome");
     for(let i = 0; i < (array.length / 2); i++){
+        console.log("checking " + i + " " + array[i] + array[array.length-i-1]);
         if(array[i] === array[array.length-i-1])
         {
-            console.log("checking");
+            isPalindrome = true;
         }
         else
         {
-            console.log("This is not a palindrome");
+            isPalindrome = false;
             return;
         }        
     }
-    console.log("This is a palindrome");
+    isPalindrome?console.log("This is a palindrome"):console.log("This is not a palindrome");
 }
-
-evaluatePalindrome(['a','b','c','d','d', 'g', 'c','b','a']);
