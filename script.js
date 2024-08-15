8f48f9d76ff77331f470514d79c7d8ad09e2bb55
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-08-14 23:40:43 -0400

    Added initial evaluatePalindrome function

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ checkBtn.addEventListener("click", () => {
         alert("Please fill in a value");
     }
 });
+
+/* 
+1. Validate the text input
+2. convert the input to all lowercase and remove whitespace
+3. place each letter in an array
+4. check front and back, if the letters are the same continue 
+    otherwise the input is not a palindrome
+*/
+
+function evaluatePalindrome(array){
+    for(let i = 0; i < (array.length / 2); i++){
+        if(array[i] === array[array.length-i-1])
+        {
+            console.log("checking");
+        }
+        else
+        {
+            console.log("This is not a palindrome");
+            return;
+        }        
+    }
+    console.log("This is a palindrome");
+}
+
+evaluatePalindrome(['a','b','c','d','d', 'g', 'c','b','a']);
