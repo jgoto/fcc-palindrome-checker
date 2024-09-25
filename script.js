2946d887041fdffecee22fa21e5fd0d9aff326a1
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-09-24 20:25:45 -0400

    Refactored the code to remove the global inputArray object and streamline the evaluation function to return early if the string is not a palindrome

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 const result = document.getElementById("result-text");
-const inputArray = [];
 
 /* Ensure user enters a value for the text input 
 when the submit button is clicked */
@@ -12,38 +11,27 @@ checkBtn.addEventListener("click", () => {
     }
     else{
         const input = textInput.value.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
-        input.split('').forEach(char => {
-            inputArray.push(char);
-        });
-        const isPalindrome = evaluatePalindrome(inputArray);
+        const isPalindrome = evaluatePalindrome(input.split(''));
         isPalindrome?result.innerText = textInput.value + " is a palindrome" : result.innerText = textInput.value + " is not a palindrome";
         resetApplication();
     }
 });
 
 function evaluatePalindrome(array){
-    let isEqual = false;
+    let isEqual = true;
     console.log("checking to see if this is a palindrome");
     for(let i = 0; i < (array.length / 2); i++){
         console.log("checking " + i + " " + array[i] + array[array.length-i-1]);
-        if(array[i] === array[array.length-i-1])
+        if(array[i] !== array[array.length-i-1])
         {
-            isEqual = true;
+            return false;
         }
-        else
-        {
-            isEqual = false;
-            break;
-        }        
     }
     return isEqual;
 }
 
 function resetApplication(){
     textInput.value ="";
-    while(inputArray.length>0){
-        inputArray.pop();
-    }
 }
 
 // module.exports = evaluatePalindrome
